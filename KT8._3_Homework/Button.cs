2b92d597549efc4ab603f9debbb22b4089a9f2be
Maksim <bobreczovm@inbox.ru>
2023-10-19 11:27:04 +0300

    Добавьте файлы проекта.

---
+++ KT8._3_Homework/Button.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT8._3_Homework
+{
+    /// <summary>
+    /// Класс Button
+    /// </summary>
+    public class Button
+    {
+        private string text;
+        private EventHandler clickEvent;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        /// <summary>
+        /// Событие EventHandler Click
+        /// </summary>
+        public event EventHandler Click
+        {
+            add
+            {
+                if (clickEvent == null || clickEvent.GetInvocationList().Length < 3)
+                {
+                    if (clickEvent != null && Array.IndexOf(clickEvent.GetInvocationList(), value) != -1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        Console.WriteLine("Подписчик уже присутствует");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+
+                    }
+                    else
+                    {
+                        clickEvent += value;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+
+                        Console.WriteLine("Подписка на событие Click");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Максимальное количество подписчиков достигнуто");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+
+                }
+            }
+            remove
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+                clickEvent -= value;
+                Console.WriteLine("Отписка от события Click");
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+            }
+        }
+
+        /// <summary>
+        /// Нажатиек кнопки
+        /// </summary>
+        public void OnClick()
+        {
+            clickEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+
+    }
+}
---
+++ KT8._3_Homework/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace KT8._3_Homework
+{
+    class Program
+    {
+        /// <summary>
+        /// Напишите класс Button, который имеет свойство Text типа string и событие типа EventHandler с именем Click, которое возникает при нажатии на кнопку. 
+        /// Затем напишите аксессоры для этого события: add и remove, которые контролируют, сколько подписчиков может иметь это событие.
+        /// Например, вы можете ограничить максимальное количество подписчиков до трех или запретить добавлять один и тот же подписчик дважды.
+        /// Затем напишите несколько методов, которые могут быть подписаны на это событие, например, выводящие на консоль текст кнопки или меняющие его цвет. 
+        /// Продемонстрируйте работу этих методов в методе Main.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Button button = new Button();
+
+            button.Click += Button_Click;
+            button.Click += Button_ChangeColor;
+            button.Click += Button_OutputText;
+
+            button.Text = "Нажми меня!";
+            button.OnClick();
+
+            button.Click -= Button_Click;
+            button.Click -= Button_ChangeColor;
+            button.Click -= Button_OutputText;
+
+            button.OnClick();
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Кнопка нажата
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void Button_Click(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Нажата кнопка!");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+
+        /// <summary>
+        /// Меняет цвет
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void Button_ChangeColor(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Изменение цвета кнопки");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+
+        /// <summary>
+        /// Текст кнопки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void Button_OutputText(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Button button = (Button)sender;
+            Console.WriteLine($"Текст кнопки: {button.Text}");
+        }
+    }
+}
